b0edc47d408be18129edaad10f900016071f555b
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-20 22:39:22 +0530

    Create Admin Dashboard

---
+++ app/admin/addProduct/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <form className="pt-5 px-5 sm:pt-12 sm:pl-16">
+        <p className="text-xl">Upload thumbnail</p>
+      </form>
+    </>
+  );
+};
+
+export default page;
