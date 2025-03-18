371fa7f5d1b7fd3c08e4845665ceb4cfe722fdc3
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-03-18 20:17:07 +0530

    Added User Authentication
    - Protected all the Admin routes.
    - Implemented Clerk Authentication only for Google Login.

--- app/page.js
+++ app/page.js
@@ -5,14 +5,15 @@ import Header from "@/Components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
+    <ClerkProvider>
       <ToastContainer theme="dark" />
       <Header />
       <BlogList />
       <Footer />
-    </>
+    </ClerkProvider>
   );
 }
