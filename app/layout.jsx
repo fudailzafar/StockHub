10ed844a288dc50d3ad4d27104baf14e7ea3bcd6
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-04-05 21:16:38 +0530

    Added Open Graph Protocol

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,29 @@ const inter = Inter({
 export const metadata = {
   title: "StockHub",
   description: "Your one-stop guide to learn about finance!",
-  icons: "/chart-no-axes-column-increasing.png",
+  icons: "/stockhub.png",
+  openGraph: {
+    title: "StockHub",
+    description: "Your one-stop guide to learn about finance!",
+    url: "https://stockhub.fun",
+    siteName: "StockHub",
+    images: [
+      {
+        url: "/og-stockhub.png",
+        width: 1200,
+        height: 630,
+        alt: "StockHub Logo and Tagline",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "StockHub",
+    description: "Your one-stop guide to learn about finance!",
+    images: ["/og-stockhub.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
