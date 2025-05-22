9f536ed83bbbf04fc11f12edb7a170a7c3013615
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-05-22 20:05:50 +0530

    SEO & Assets
    - Updated SEO
    - Removed assets folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,19 +4,24 @@ import "./globals.css";
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
 });
 
 export const metadata = {
+  metadataBase: new URL("https://stockhub.fun"),
   title: "StockHub | Your one-stop guide to learn about finance!",
   description:
     "People think that investing in stock markets is risky, deadly, and what not! But StockHub is here to teach you how to buy the castle you always wanted! Welcome to the MoneyLand!",
-  icons: "https://stockhub.fun/favicon.ico",
+  icons: {
+    icon: "https://stockhub.fun/favicon.ico",
+  },
   keywords:
     "finance, investing, stock market, learn stocks, StockHub, financial education, personal finance",
-  author: "Fudail",
+  authors: [{ name: "Fudail" }],
   openGraph: {
     title: "StockHub | Your one-stop guide to learn about finance!",
-    description: "People think that investing in stock markets is risky, deadly, and what not! But StockHub is here to teach you how to buy the castle you always wanted! Welcome to the MoneyLand!",
+    description:
+      "People think that investing in stock markets is risky, deadly, and what not! But StockHub is here to teach you how to buy the castle you always wanted! Welcome to the MoneyLand!",
     url: "https://stockhub.fun",
     siteName: "StockHub",
     images: [
@@ -33,20 +38,36 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "StockHub | Your one-stop guide to learn about finance!",
-    description: "People think that investing in stock markets is risky, deadly, and what not! But StockHub is here to teach you how to buy the castle you always wanted! Welcome to the MoneyLand!",
+    description:
+      "People think that investing in stock markets is risky, deadly, and what not! But StockHub is here to teach you how to buy the castle you always wanted! Welcome to the MoneyLand!",
     images: ["https://stockhub.fun/banner.png"],
   },
   alternates: {
     canonical: "https://stockhub.fun",
   },
-  robots: "index, follow",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: "NusNWY1SGmpzIbgDgWcPHUvhtkD18twqsq2XKO9zcm4",
+    yandex: "7141cd6363c4bc9a",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} font-sans antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
